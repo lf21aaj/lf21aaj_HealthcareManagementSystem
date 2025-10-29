9d1e614a76cec75b3f1b561fc37551288c8c2311
lf21aaj <lf21aaj@herts.ac.uk>
2025-10-29 10:40:05 +0000

    Author: Lynden Fenn Feature: add Clinician class with fields and getters

--- src/Clinician.java
+++ src/Clinician.java
@@ -1,2 +1,49 @@
-public class Clinician {
+public class Clinician
+{
+    private String id;
+    private String name;
+    private String speciality;
+    private String role;
+    private String facility;
+
+    public Clinician(String id, String name, String speciality, String role, String facility)
+    {
+        this.id = id;
+        this.name = name;
+        this.speciality = speciality;
+        this.role = role;
+        this.facility = facility;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSpeciality()
+    {
+        return speciality;
+    }
+
+    public String getRole()
+    {
+        return role;
+    }
+
+    public String getFacility()
+    {
+        return facility;
+    }
+
+    @Override
+    public String toString()
+    {
+        String spec = (speciality == null || speciality.isBlank()) ? "" : " (" + speciality +")";
+        return name + " - " + role + spec;
+    }
 }
